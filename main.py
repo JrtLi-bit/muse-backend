9c1aaeecc1ea046a36821b065fd3608cde66e27a
JrtLi-bit <jarrett0603@gmail.com>
2026-04-30 02:06:49 -0400

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ if not api_key:
     print("Warning: GEMINI_API_KEY environment variable is missing.")
 
 client = genai.Client(api_key=api_key)
-MODEL_ID = 'gemini-3-flash-preview'
+MODEL_ID = 'gemini-3.1-flash-lite'
 
 class CanvasData(BaseModel):
     image_data: str
